1ffb61f8ec31b4d9b43bcc4ef581cbb03c41ed43
James Peckham <jdpeckham@gmail.com>
2019-07-27 21:41:51 -0500

    updated tests

--- test/testOauth.js
+++ test/testOauth.js
@@ -3,16 +3,23 @@ const expect = require('chai').expect;
 const config = require('../config')
 const reddit = require('../reddit');
 
-describe('oauth', function() {
-    describe('reddit', function() {
-        it('should not be undefined', function() {
-            expect(reddit).is.not.undefined
-        })
-        describe('access_token', function() {
-            it('should return valid token', async function() {
-                const {access_token} = await reddit.access_token(config)
+describe('reddit', function() {
+    describe('access_token', function() {
+        it('should return valid token', function() {
+            return reddit.access_token(config).then(function(access_token){
                 expect(access_token).to.not.be.undefined
             })
+            
         })
-    })
-});
+    })  
+
+    describe('me.name', function(){
+        it('should be JheredParnell', async function(){
+            let {access_token} = await reddit.access_token(config)
+            return reddit.me({access_token}).then(function(respData){
+                expect(respData.name).to.equal('JheredParnell')
+            })
+        })
+    }) 
+  
+})
